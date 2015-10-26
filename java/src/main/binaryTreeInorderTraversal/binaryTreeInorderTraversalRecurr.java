73931bceecd30a05ddb0f1086b28a3ded16653c0
Dan Fan <dfan@appnexus.com>
2015-10-25 20:35:46 -0400

    inorder traversal

---
+++ java/src/main/binaryTreeInorderTraversal/binaryTreeInorderTraversalRecurr.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        if(root == null)
+        {
+            return result;
+        }
+        result.addAll(inorderTraversal(root.left));
+        result.add(root.val);
+        result.addAll(inorderTraversal(root.right));
+        return result;
+    }
+}
