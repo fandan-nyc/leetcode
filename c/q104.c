b6883645181c91400697a335b0447be9f08d2196
Dan Fan <dfan@appnexus.com>
2015-12-21 23:41:54 -0500

    question 104 in c

---
+++ c/q104.c
@@ -0,0 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int maxDepth(struct TreeNode* root) {
+    if(root == NULL)
+        return 0;
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+    return 1 + (left > right ? left: right);
+}
