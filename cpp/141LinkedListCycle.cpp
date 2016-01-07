ed0664e81682916ece43ed579ee6127e507de777
Xiaolong Zhang <notadish@gmail.com>
2016-01-07 17:41:17 -0500

    review

--- cpp/141LinkedListCycle.cpp
+++ cpp/141LinkedListCycle.cpp
@@ -1,8 +1,12 @@
 /*
- Given a linked list, determine if it has a cycle in it.
+    File Name: 141LinkedListCycle.cpp
+    Xiaolong Zhang
 
-Follow up:
-Can you solve it without using extra space? 
+    Question:
+    Given a linked list, determine if it has a cycle in it.
+
+    Follow up:
+    Can you solve it without using extra space? 
 */
 
 /**
