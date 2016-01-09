2d0ee29dfffb236519075cda29fca1938026a5d3
Xiaolong Zhang <notadish@gmail.com>
2016-01-09 15:27:26 -0500

    sync

---
+++ cpp/035SearchInsertPosition.cpp
@@ -0,0 +1,28 @@
+/*
+    File Name: 035SearchInsertPosition.cpp
+    Xiaolong Zhang
+
+    Question:
+    Given a sorted array and a target value, 
+    return the index if the target is found. 
+    If not, return the index where it would 
+    be if it were inserted in order.
+
+	You may assume no duplicates in the array.
+
+	Here are few examples.
+	[1,3,5,6], 5 → 2
+	[1,3,5,6], 2 → 1
+	[1,3,5,6], 7 → 4
+	[1,3,5,6], 0 → 0
+*/
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] >= target)
+            	return i;
+        }
+        return nums.size();
+    }
+};
