dd3c6912f507a439d338afd692ebd36b4b2c6fab
Dan Fan <fandan.nyc@gmail.com>
2016-01-14 21:23:53 -0500

    Update MergeSortedLinkedListIter.java

--- java/src/main/mergeSortedLinkedList/MergeSortedLinkedListIter.java
+++ java/src/main/mergeSortedLinkedList/MergeSortedLinkedListIter.java
@@ -8,35 +8,24 @@
  */
 public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode head = new ListNode(0);
-        ListNode tmp =  head ;
-        while(l1 != null || l2 != null)
+        ListNode result = new ListNode(0);
+        ListNode tmp = result;
+        while(l1 != null && l2 != null)
         {
-            if(l1 == null)
-            {
-                tmp.next = l2;
-                l2 = null;
-            }
-            else if(l2 == null) 
-            {
+            if(l1.val < l2.val){
                 tmp.next = l1;
-                l1 = null;
+                l1=l1.next;
             }
-            else
-            {
-                if(l1.val < l2.val)
-                {
-                    tmp.next = l1;
-                    l1 = l1.next;
-                }
-                else
-                {
-                    tmp.next = l2;
-                    l2 = l2.next;
-                }
-                tmp = tmp.next;
+            else {
+                tmp.next = l2;
+                l2=l2.next;
             }
+            tmp = tmp.next;
         }
-        return head.next;
+        if(l1 == null)
+            tmp.next = l2;
+        else 
+            tmp.next = l1;
+        return result.next;
     }
 }
