7a2d953e95993a95c1db74f4fa30f18486067a1e
Dan Fan <dfan@appnexus.com>
2016-01-21 20:18:40 -0500

    revert binary tree

---
+++ java/src/main/revertBinaryTree/recursive.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return root;
+        }
+        TreeNode tmp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(tmp);
+        return root;
+        
+    }
+}
