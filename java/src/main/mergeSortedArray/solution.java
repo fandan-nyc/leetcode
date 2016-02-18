b2b9215fa9abc9f1c405d1c6db1f46f4647be971
Dan Fan <dfan@appnexus.com>
2016-02-18 17:14:48 -0500

    merged sorted array

---
+++ java/src/main/mergeSortedArray/solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        while(m > 0 && n>0){
+            nums1[m+n-1] = Math.max(nums1[m-1] , nums2[n-1]);
+            if(nums1[m-1] > nums2[n-1]){
+                m--;
+            }else{
+                n--;
+            }
+        }
+        for( int i =0; i < n;i++){
+            nums1[i] = nums2[i];
+        }
+    }
+}
