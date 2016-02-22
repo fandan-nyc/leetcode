70efe7a469c908ad2a5a118b6441840f12092621
Dan Fan <fandan.nyc@gmail.com>
2016-02-22 06:22:23 -0500

    Update BinaryTreePreorderTraversalRecur.java

--- java/src/main/binaryTreePreorderTraversal/BinaryTreePreorderTraversalRecur.java
+++ java/src/main/binaryTreePreorderTraversal/BinaryTreePreorderTraversalRecur.java
@@ -19,3 +19,30 @@ public class Solution {
         return result;
     }
 }
+
+// the solution above is bad too, because using recursive you need to recreate a new result and the cost of memory is huge. 
+// try the following one: 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list =new ArrayList<Integer>();
+        helper(root, list);
+        return list;
+    }
+    
+    private void helper(TreeNode root, List<Integer> list){
+        if(root != null){
+            list.add(root.val);
+            helper(root.left, list);
+            helper(root.right,list);
+        }
+    }
+}
