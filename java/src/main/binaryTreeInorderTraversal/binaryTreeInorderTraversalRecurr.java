593e0883467b7d5958057821d9c946a83277f651
Dan Fan <fandan.nyc@gmail.com>
2016-02-22 09:06:12 -0500

    Update binaryTreeInorderTraversalRecurr.java

--- java/src/main/binaryTreeInorderTraversal/binaryTreeInorderTraversalRecurr.java
+++ java/src/main/binaryTreeInorderTraversal/binaryTreeInorderTraversalRecurr.java
@@ -20,3 +20,33 @@ public class Solution {
         return result;
     }
 }
+
+// better way 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>(); 
+        helper(root, list);
+        return list;
+    }
+    
+    private void helper(TreeNode root, List<Integer> list){
+        if(root == null){
+            return;
+        }
+        else{
+            helper(root.left,list); 
+            list.add(root.val);
+            helper(root.right, list);
+        }
+    }
+    
+}
