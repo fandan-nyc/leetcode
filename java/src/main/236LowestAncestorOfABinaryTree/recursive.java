622e4a67fdd7c52f4a6e1ef7fc34d2affbc9d96c
Dan Fan <dfan@appnexus.com>
2016-06-14 22:44:03 -0400

    q 236

---
+++ java/src/main/236LowestAncestorOfABinaryTree/recursive.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null){
+            return root;
+        }
+        if(root == p){
+            return p;
+        }
+        if(root == q){
+            return q;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left != null && right != null){
+            return root;
+        }else{
+            return left == null? right: left;
+        }
+    }
+}
