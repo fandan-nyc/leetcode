6b36114ce0c4f488b58faf6923dfb83f7fab9b2f
Dan Fan <dfan@appnexus.com>
2016-07-08 01:15:49 -0400

    q 124

---
+++ java/src/main/124BinaryTreeMaxPathSum/solution.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    private int max = 0;
+    public int maxPathSum(TreeNode root) {
+        maxPathSumWithCurrentNode(root);
+        return max;
+    }
+    
+    private int maxPathSumWithCurrentNode(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int leftMax  = Math.max(maxPathSumWithCurrentNode(root.left), 0);
+        int rightMax = Math.max(maxPathSumWithCurrentNode(root.right), 0);
+        int currMax = (leftMax + rightMax + root.val);
+        max = max > currMax ? max: currMax;
+        return Math.max(leftMax, rightMax) + root.val;
+    }
+    
+}
