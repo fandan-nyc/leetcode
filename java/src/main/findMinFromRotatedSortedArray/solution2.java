5bf22d87b08dc00923c5d51122fec08f8fce092b
Dan Fan <fandan.nyc@gmail.com>
2016-07-08 19:51:55 -0400

    Update solution2.java

--- java/src/main/findMinFromRotatedSortedArray/solution2.java
+++ java/src/main/findMinFromRotatedSortedArray/solution2.java
@@ -2,16 +2,13 @@ public class Solution {
     public int findMin(int[] nums) {
         int start  = 0; 
         int end = nums.length-1;
-        while(start < end){
+        while(start  < end){
             int middle = (start + end) /2 ; 
-            if( middle >= 1 && nums[middle] < nums[middle-1]){
-                return nums[middle];
-            }
-            else if( nums[middle] >= nums[start] && nums[middle] >= nums[end]){
+            if( nums[middle] > nums[end]){
                 start = middle +1 ; 
             }
             else{
-                end = middle -1;
+                end = middle;
             }
         }
         return nums[start];
