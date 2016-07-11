1b57e5022aa9ab0e8b78953af6c03c6bf30ebb89
Dan Fan <fandan.nyc@gmail.com>
2016-07-11 03:13:09 -0400

    Create iter2.java

---
+++ java/src/main/mergeSortedLinkedList/iter2.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode dummy = head;
+        while(l1 != null || l2 != null){
+            if(l1 == null){
+                dummy.next = l2;
+                l2 = l2.next;
+            }else if(l2 == null){
+                dummy.next = l1;
+                l1 = l1.next;
+            }else if(l1.val <= l2.val){
+                dummy.next = l1;
+                l1 = l1.next;
+            }else{
+                dummy.next = l2;
+                l2 = l2.next;
+            }
+            dummy = dummy.next;
+        }
+        return head.next;
+    }
+}
