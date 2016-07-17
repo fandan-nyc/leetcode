ba33e441e07f785f45de28330bce82142c221630
Dan Fan <fandan.nyc@gmail.com>
2016-07-17 16:01:56 -0400

    Update solution.java

--- java/src/main/3sumClosest/solution.java
+++ java/src/main/3sumClosest/solution.java
@@ -1,40 +1,34 @@
 public class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        Arrays.sort(nums);
-        if(nums == null || nums.length <3)
+        if(nums ==  null || nums.length == 0){
             return 0;
-        int tmpPos = Integer.MAX_VALUE;
-        int tmpNeg = Integer.MIN_VALUE+1;
-        int low;
-        int high;
-        int sum;
-        for(int i = 0; i < nums.length -2;i++){
-            low = i+1;
-            high = nums.length -1;
-            while(low < high){
-                sum = nums[i] + nums[low] + nums[high];
-                if(sum == target)
-                    return target;
-                else if(sum < target){
-                    tmpNeg = Math.max(tmpNeg,sum - target);
-                    while( low < high && nums[low] == nums[low+1]){
-                        low++;
-                    }
-                    low++;
-                }
-                else{
-                    tmpPos = Math.min(tmpPos, sum - target);
-                    while( low < high && nums[high] == nums[high-1]){
-                        high--;
-                    }
-                    high--;
-                }
-            
-            while(i < nums.length-2 && nums[i] == nums[i+1])
-            {
-                i++;
+        }
+        if(nums.length <=3){
+            int result = 0 ;
+            for(int i = 0;  i < nums.length; i++){
+                result += nums[i];
             }
-        }}
-        return  target+ ((Math.abs(tmpPos) > Math.abs(tmpNeg))? tmpNeg : tmpPos);
+            return result;
+        }
+        Arrays.sort(nums);
+        int large = Integer.MAX_VALUE;
+        int small = Integer.MIN_VALUE;
+        for(int i = 0; i < nums.length-2; i++){
+            int start  = i+1;
+            int end = nums.length-1;
+            while(start < end){
+                int curr = nums[i] + nums[start] + nums[end];
+                if(curr == target){
+                    return target;
+                }else if(curr > target){
+                    large = Math.min(large, curr-target);
+                    end--;
+                }else if(curr < target){
+                    small = Math.max(small, curr-target);
+                    start++;
+                }
+            }
+        }
+        return target + ((large+ small ) > 0 ? small : large);
     }
 }
