9f34b515f4d3a902a5a52c266aff36afc06bae58
Dan Fan <fandan.nyc@gmail.com>
2016-07-17 06:21:30 -0400

    Update twoSumHashMap.java

--- java/src/main/twoSum/twoSumHashMap.java
+++ java/src/main/twoSum/twoSumHashMap.java
@@ -5,9 +5,9 @@ public class Solution {
             return null;
         for(int i = 0 ; i< nums.length ; i++){
             if(map.containsKey(nums[i]))
-                return new int[]{map.get(nums[i]),i+1};
+                return new int[]{map.get(nums[i]),i};
             else 
-                map.put(target - nums[i], i+1);
+                map.put(target - nums[i], i);
         }
         return null;
     }
