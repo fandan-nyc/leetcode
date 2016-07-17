61c956d23c9d0bd4b7b527a1c467d9f6d2faae1d
Dan Fan <fandan.nyc@gmail.com>
2016-07-17 06:59:17 -0400

    Update solution.java

--- java/src/main/strToInt/solution.java
+++ java/src/main/strToInt/solution.java
@@ -1,43 +1,18 @@
 public class Solution {
     public int myAtoi(String str) {
-        if(str == null)
-            return 0;
         str = str.trim();
-        if(str.length() == 0)
-            return 0;
-        long result  = 0; 
-        int startPosition = 0 ;
-        boolean positive = true;
-        char first = str.charAt(0);
-        if(!isSign(first) && !isNumeric(first))
-            return (int)result;
-        else if(first == '+'){
-            startPosition = 1;
-        }
-        else if(first == '-' ){
-            startPosition = 1;
-            positive = false;
-        }
-        for( int i = startPosition;i < str.length(); i ++){
-            char j = str.charAt(i);
-            if(isNumeric(j))
-                result = result * 10 + (positive ? ( j - '0' ): ('0' - j));
-            else 
-                return (int) result;
-            if(result > Integer.MAX_VALUE)
-                return Integer.MAX_VALUE;
-            else if ( result < Integer.MIN_VALUE)
-                return Integer.MIN_VALUE;
+        long result = 0;
+        boolean neg = false;
+        for (int i = 0; i < str.length(); i ++ ) {
+            if (i == 0 && (str.charAt(i) == '-' || str.charAt(i) == '+')) {
+                neg = str.charAt(i) == '-' ? true : false;
+                continue;
+            }
+            if (str.charAt(i) < '0' || str.charAt(i) > '9') break;
+            result = neg ? result * 10 - ( str.charAt(i) - '0' ) : result * 10 + ( str.charAt(i) - '0' );
+            if (result > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            if (result < Integer.MIN_VALUE) return Integer.MIN_VALUE;
         }
         return (int)result;
-        
-    }
-    
-    private boolean isNumeric( char i){
-        return i >= '0' && i <= '9';
-    }
-    
-    private boolean isSign(char i){
-        return i == '+' || i == '-';
     }
 }
