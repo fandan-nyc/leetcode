a38bbfac18f55737861c66a5e5a9f1d1fbbd868b
Dan Fan <fandan.nyc@gmail.com>
2016-07-18 09:12:26 -0400

    Update bruteForce.java

--- java/src/main/strStr/bruteForce.java
+++ java/src/main/strStr/bruteForce.java
@@ -1,25 +1,18 @@
-pubic class Solution{
-	public int strStr(String text, String str){
-		if(text == null || str == null)
-			return -1;
-		if(str.equals(""))
-			return 0;
-		if(text.equals(""))
-			return -1;
-		if(str.length() > text.length())
-			return -1;
-		int strPos = 0;
-		for(int i = 0; i <= text.length()-str.length(); i++){
-			while(strPos < str.length() && text.charAt(i+strPos) == str.charAt(strPos)){
-				strPos++;
-			}
-			if(strPos == str.length()){
-				return i;
-			}
-			else{
-				strPos =0;
-			}
-		}
-		return -1;
-	}
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        //brute force
+        for(int i = 0; i <= haystack.length() - needle.length(); i ++){
+        	int k = i;
+        	for(int j = 0; j < needle.length();j++){
+        		if(haystack.charAt(k) != needle.charAt(j)){
+        			break;
+        		}
+        		k++;
+        	}
+        	if(k  - i == needle.length()){
+        		return i;
+        	}
+        }
+        return -1;
+    }
 }
