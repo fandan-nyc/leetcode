fbbc55c87169599adb04d5c65fa5dcf66f4db113
Dan Fan <fandan.nyc@gmail.com>
2016-07-23 17:45:34 -0400

    Update solution.java

--- java/src/main/43MultiplyStrings/solution.java
+++ java/src/main/43MultiplyStrings/solution.java
@@ -1,77 +1,58 @@
 public class Solution {
     public String multiply(String num1, String num2) {
-        if(num1.equals("0") || num2.equals("0")){
-            return "0";
-        }else if(num1.equals("1")){
-            return num2;
-        }else if(num2.equals("1")){
-            return num1;
-        }
-        
-        List<String> store = new ArrayList<String>();
+         if(num2.equals("0") || num1.equals("0")){
+             return "0";
+         }
+        List<String> result = new ArrayList<String>();
+        String zero = "";
         for(int i = num2.length()-1; i >= 0; i--){
-            store.add(multiplySingle(num1, num2.charAt(i), num2.length() - i - 1));
+            result.add( multiply(num1, num2.charAt(i)) + zero);
+            zero += "0";
         }
-        String result = "";
-        for(String i: store){
-            result = addString(result, i);
+        String answer = "0";
+        for(String i: result){
+            answer = add(answer, i);
         }
-        return result;
+        return answer;
+        
     }
     
-    private String multiplySingle(String num1, char single, int zeros){
+    private String multiply(String a, char b){
         int carryOver = 0;
-        StringBuilder sb = new StringBuilder();
-        for(int i = num1.length()-1; i >= 0; i--){
-            int left = num1.charAt(i) - '0';
-            int right = single - '0';
-            int result = left * right + carryOver;
-            int digit = result % 10; 
-            carryOver = result/10;
-            sb.insert(0, digit);
+        StringBuilder sb =  new StringBuilder();
+        for(int i= a.length()-1; i >= 0; i--){
+            int curr = (a.charAt(i) - '0') * (b - '0') + carryOver;
+            sb.insert(0, curr%10);
+            carryOver = curr/10;
         }
-        if(carryOver != 0){
+        if(carryOver > 0){
             sb.insert(0, carryOver);
-        }
-        while(zeros > 0){
-            sb.append('0');
-            zeros --;
         }
         return sb.toString();
     }
     
-    private String addString(String a, String b){
-        if(a.length() == 0){
-            return b;
-        }else if(b.length() == 0){
-            return a;
-        }
-        
-        int carryOver = 0; 
-        StringBuilder sb = new StringBuilder();
-        int i = 0;
-        int j = 0;
-        for( i = a.length() -1,  j = b.length()-1; i >=0 && j>=0; i--,j--){
-            int left = a.charAt(i) - '0';
-            int right = b.charAt(j) - '0';
-            int result = (left + right + carryOver);
-            sb.insert(0, result %10);
-            carryOver = result / 10;
-        }
-        while( i >= 0){
-            int result = a.charAt(i) - '0' + carryOver;
-            sb.insert(0, result %10);
-            carryOver = result /10;
+    private String add(String a, String b){
+        int i = a.length()-1; 
+        int j = b.length()-1;
+        int carryOver = 0;
+        StringBuilder sb =  new StringBuilder();
+        while( i  >= 0 || j >= 0){
+            int left = 0;
+            int right = 0;
+            if( i >= 0){
+                left = a.charAt(i) - '0';
+            }
+            if( j >=0){
+                right = b.charAt(j) - '0';
+            }
+            int curr = left + right + carryOver;
+            sb.insert(0, curr%10);
+            carryOver = curr/10;
             i--;
-        }
-        while( j >= 0){
-            int result = b.charAt(j) - '0' + carryOver;
-            sb.insert(0, result %10);
-            carryOver = result /10;
             j--;
         }
         if(carryOver > 0){
-            sb.insert(0, carryOver);
+            sb.insert(0,carryOver);
         }
         return sb.toString();
     }
