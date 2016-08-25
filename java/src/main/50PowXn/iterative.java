8f02904d6833f8bd13ed3a5324efb2a6621567b7
Dan Fan <fandan.nyc@gmail.com>
2016-08-25 19:45:10 -0400

    Update iterative.java

--- java/src/main/50PowXn/iterative.java
+++ java/src/main/50PowXn/iterative.java
@@ -1,30 +1,22 @@
 public class Solution {
+    // iterative
     public double myPow(double x, int n) {
-        if(n == 0){
-            return 1;
-        }
-        // recursive
-        boolean positive = n > 0;
-        return positive ? helper(x, (long) n) : 1/ helper(x, (long)n * -1);
-    }
-    
-    private double helper(double x, long n){
-        Stack<Double> stack = new Stack<Double>();
-        stack.push(x);
-        long count  = 1;
-        while( count <= n){
-            stack.push(stack.peek() * stack.peek());
-            count = count << 1;
-        }
-        double result = 1;
-        while(n != 0){
-            double tmp = stack.pop();
-            if(count <= n){
-                result *= tmp;
-                n -= count;
-            }
-            count = count >>> 1;
+        double result = myPowHelper(x, n);
+        if(n < 0){
+            return 1/result;
         }
         return result;
+    }
+    
+    private double myPowHelper(double x, int n) {
+        double res = 1;
+        while(n != 0){
+            if(n %2 != 0){
+                res *= x;
+            }
+            x *= x;
+            n /= 2;
+        }
+        return res;
     }
 }
