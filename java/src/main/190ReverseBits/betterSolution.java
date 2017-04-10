390772246118f7621566e9fdfa9471e17d73ec69
Dan Fan <fandan.nyc@gmail.com>
2017-04-10 13:30:06 -0400

    Update betterSolution.java

--- java/src/main/190ReverseBits/betterSolution.java
+++ java/src/main/190ReverseBits/betterSolution.java
@@ -1,7 +1,8 @@
 public class Solution {
     // you need treat n as an unsigned value
+    private int[] answer = {0,8,4,12,2,10,6,14,1,9,5,13,3,11,7,15};
+
     public int reverseBits(int n) {
-        int[] answer = {0,8,4,12,2,10,6,14,1,9,5,13,3,11,7,15};
         int tmp = 0;
         int counter = 7 ; 
         while(counter-- >= 0){
