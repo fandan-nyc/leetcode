22ca6ab14a505a188fcea966d3f1e7ca21ebd437
Dan Fan <fandan.nyc@gmail.com>
2017-04-10 19:54:21 -0400

    Update majorityElement1.java

--- java/src/main/majorityElement/majorityElement1.java
+++ java/src/main/majorityElement/majorityElement1.java
@@ -1,20 +1,11 @@
 public class Solution {
     public int majorityElement(int[] nums) {
-        Map<Integer, Integer> resultMap = new HashMap<Integer, Integer>();
-        for(int i : nums)
-        {
-            resultMap.put(i, resultMap.containsKey(i) ? resultMap.get(i) + 1: 1);
+        Map<Integer, Integer> store = new HashMap<Integer, Integer>();
+        for(int i: nums){
+            store.put(i, store.containsKey(i)? (store.get(i)+1):1);
+            if(store.get(i) > (nums.length / 2))
+                return i;
         }
-        int result = 0;
-        int max = 0;
-        for(Map.Entry<Integer, Integer> i : resultMap.entrySet())
-        {
-            if(i.getValue() > max)
-            {
-                max = i.getValue(); 
-                result = i.getKey();
-            }
-        }
-        return result ; 
+        return -1;
     }
 }
