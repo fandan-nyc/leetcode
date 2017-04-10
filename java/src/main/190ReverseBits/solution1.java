e88380a00f7fe4c299a361d374b45314d5bde439
Dan Fan <dfan@appnexus.com>
2017-04-10 13:26:10 -0400

    add answer to 190

--- java/src/main/190ReverseBits/solution1.java
+++ java/src/main/190ReverseBits/solution1.java
@@ -3,10 +3,15 @@ public class Solution {
     public int reverseBits(int n) {
         int tmp = 0 ; 
         for(int i =0; i < 32; i ++){
-            tmp = tmp <<1;
-            tmp |= (n & 1);
-            n >>= 1;
+            if((n & 1) == 1){
+                tmp = (tmp << 1) ^ 1;
+            }
+            else{
+                tmp = (tmp << 1);
+            }
+            n = n >>> 1;
         }
         return tmp;
+        
     }
 }
