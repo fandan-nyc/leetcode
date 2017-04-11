a90133e431ad47fe2c26bd073b901ffaf9a216c6
Dan Fan <fandan.nyc@gmail.com>
2017-04-10 21:23:41 -0400

    Update solution.java

--- java/src/main/389FindTheDifference/solution.java
+++ java/src/main/389FindTheDifference/solution.java
@@ -10,3 +10,16 @@ public class Solution {
         }
         return result;
     }
+}
+
+// another one
+public class Solution {
+    public char findTheDifference(String s, String t) {
+        char result = t.charAt(s.length());
+        for(int i = 0; i < s.length(); i ++){
+            result ^= s.charAt(i);
+            result ^= t.charAt(i);
+        }
+        return result;
+    }
+}
