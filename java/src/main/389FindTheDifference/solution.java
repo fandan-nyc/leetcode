7c8fd78b134510221fe31fc9b8c3e38cc091cacf
Dan Fan <dfan@appnexus.com>
2017-04-10 21:19:32 -0400

    question 389

---
+++ java/src/main/389FindTheDifference/solution.java
@@ -0,0 +1,12 @@
+
+public class Solution {
+    public char findTheDifference(String s, String t) {
+        char result = 0;
+        for(int i = 0; i < s.length(); i ++){
+            result ^= s.charAt(i);
+        }
+        for(int i = 0; i < t.length(); i ++){
+            result ^= t.charAt(i);
+        }
+        return result;
+    }
