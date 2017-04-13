f7ce1623b20629ac6115ea9e60ae364985cb9811
Dan Fan <fandan.nyc@gmail.com>
2017-04-12 21:53:07 -0400

    Update missingNumberA.java

--- java/src/main/missingNumber/missingNumberA.java
+++ java/src/main/missingNumber/missingNumberA.java
@@ -1,12 +1,18 @@
 public class Solution {
     public int missingNumber(int[] nums) {
-        int largest  = nums.length ;
-        int sum = (largest+1) * (largest) /2; 
-        int tmp =0 ; 
-        for(int i: nums)
-        {
-            tmp += i;
+        int result = (int)(zeroToN(nums.length) - sum(nums));
+        return result;
+    }
+    
+    private long zeroToN(int n){
+        return n * (n+1)/2;
+    }
+    
+    private long sum(int[] nums){
+        long result = 0; 
+        for(int i: nums){
+            result += i;
         }
-        return sum - tmp ;
+        return result;
     }
 }
