269608a80ac4d5c1c5e7fe94cfbff17e45f3fe35
Dan Fan <fandan.nyc@gmail.com>
2017-04-13 22:11:28 -0400

    Update solution.java

--- java/src/main/deleteNodeInALinkedList/solution.java
+++ java/src/main/deleteNodeInALinkedList/solution.java
@@ -8,11 +8,8 @@
  */
 public class Solution {
     public void deleteNode(ListNode node) {
-        if(node.next == null)
-            node = null;
-        else{
-            node.val = node.next.val;
-            node.next = node.next.next;
-        }
+        // cannot be tail
+        node.val = node.next.val;
+        node.next = node.next.next;
     }
 }
