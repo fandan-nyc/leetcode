68cd3117fd0230473a62e153b7563f037b7b9caa
Dan Fan <fandan.nyc@gmail.com>
2017-04-13 21:20:09 -0400

    Update solution.java

--- java/src/main/maxProductOfWordLength/solution.java
+++ java/src/main/maxProductOfWordLength/solution.java
@@ -1,21 +1,25 @@
 public class Solution {
     public int maxProduct(String[] words) {
-    if(words == null || words.length <= 1){
-        return 0;
-    }
-    int[] mark = new int[words.length];
-    for(int i= 0; i < words.length; i ++){
-        for(int j = 0; j < words[i].length(); j++){
-            mark[i] |= 1 << (words[i].charAt(j) - 'a');
+        int[] wordsCode = new int[words.length];
+        for(int i = 0 ; i < words.length; i ++){
+            wordsCode[i] = stringToInt(words[i]);
         }
-    }
-    int max = 0;
-    for( int i = 0; i < words.length -1; i++)
-        for(int j = i; j < words.length; j++ ){
-            if( (mark[i] & mark[j])== 0){
-                max = Math.max(words[i].length() * words[j].length(), max);
+        int max = 0 ;
+        for(int i = 0; i < words.length-1; i ++){
+            for(int j = i+1; j < words.length; j++){
+                if((wordsCode[i] & wordsCode[j]) == 0){
+                    max = Math.max(max, words[i].length() * words[j].length());
+                }
             }
+        }
+        return max;
     }
-    return max;
+    
+    private int stringToInt(String input){
+        int result  = 0;
+        for(int i = 0; i < input.length(); i++){
+            result |= (1 << (input.charAt(i) -'a'));
+        }
+        return result;
     }
 }
