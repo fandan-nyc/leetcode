1334392377c4e29064dab72d80fcc9b63a971234
Dan Fan <dfan@appnexus.com>
2017-05-31 10:16:05 -0400

    question 245

---
+++ java/src/main/245ShortestWordDistIII/solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int shortestWordDistance(String[] words, String word1, String word2) {
+        int min = Integer.MAX_VALUE;
+        int a = -1; 
+        int b = -1; 
+        for(int i = 0; i < words.length; i++){
+            if(words[i].equals(word1) == false || words[i].equals(word2) == false){
+                continue;
+            }
+            if(words[i].equals(word1)){
+                if(b != -1){
+                    
+                }
+            }
+        }
+    }
+}
