4030dff62f3eebda197a028a3556470bd445dc9e
Dan Fan <fandan.nyc@gmail.com>
2017-06-06 19:12:35 -0400

    Update SymmetricTreeRecurr.java

--- java/src/main/symmetricTree/SymmetricTreeRecurr.java
+++ java/src/main/symmetricTree/SymmetricTreeRecurr.java
@@ -9,35 +9,19 @@
  */
 public class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if(root == null)
-        {
+        if(root == null){
             return true;
         }
-       
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        stack.push(root.left);
-        stack.push(root.right);
-        while(!stack.isEmpty())
-        {
-            TreeNode left = stack.pop();
-            TreeNode right =  stack.pop();
-            if(left == null && right == null )
-            {
-                continue;
-            }
-            else if (left == null || right == null)
-            {
-                return false;
-            } 
-            else if(left.val != right.val)
-            {
-                return false;
-            }
-            stack.push(left.right);
-            stack.push(right.left);
-            stack.push(left.left);
-            stack.push(right.right);
+        return helper(root.left, root.right);
+    }
+    
+    private boolean helper(TreeNode a, TreeNode b){
+        if(a != null && b != null){
+            return a.val == b.val && helper(a.left, b.right) && helper(a.right, b.left);
+        }else if(a != null || b != null){
+            return false;
+        }else{
+            return true;
         }
-        return true;
     }
 }
