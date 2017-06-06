f7a3d92ba07352aff2c486ea5f1db1f7fb519780
Dan Fan <dfan@appnexus.com>
2017-06-06 19:34:53 -0400

    101 easy

--- java/src/main/symmetricTree/SymmetricTreeIter.java
+++ java/src/main/symmetricTree/SymmetricTreeIter.java
@@ -9,23 +9,25 @@
  */
 public class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if(root == null)
-        {
-            return true;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(root);
+        while(!queue.isEmpty()){
+            TreeNode left = queue.remove();
+            TreeNode right = queue.remove();
+            if(left == null && right == null){
+                continue;
+            }else if(left == null || right == null){
+                return false;
+            }else if(left.val != right.val){
+                return false;
+            }else{
+                queue.add(left.left);
+                queue.add(right.right);
+                queue.add(left.right);
+                queue.add(right.left);
+            }
         }
-        return symmetricSubTree(root.left, root.right);
-       
-    }
-    private boolean symmetricSubTree(TreeNode left, TreeNode right)
-    {
-        if(left == null && right == null)
-        {
-            return true;
-        }
-        else if( left == null || right == null)
-        {
-            return false;
-        }
-        return (left.val == right.val) && symmetricSubTree(left.right, right.left) && symmetricSubTree( left.left, right.right);
+        return true;
     }
 }
