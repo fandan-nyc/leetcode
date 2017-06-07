a49e75ea4d312222bde7ca62fd8d7b93b13fab49
Dan Fan <fandan.nyc@gmail.com>
2017-06-06 20:25:02 -0400

    Update FactorialTrailingZero.java

--- java/src/main/factorialTrailingZero/FactorialTrailingZero.java
+++ java/src/main/factorialTrailingZero/FactorialTrailingZero.java
@@ -1,5 +1,9 @@
 public class Solution {
     public int trailingZeroes(int n) {
+        // for example, 100, we have 100/5 = 20. every 20 numbers, we have x = 5*k;
+        // 100/25 = 4; there are4 numbers who are the k*25
+        // the total number of 5s we have are 
+        // 100/5 + 100/25 + 100/125 .... 
         int count  = 0; 
         while(n/5  !=0)
         {
