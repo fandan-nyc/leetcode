f581c731f42e811c4fe81f402ca6fd687a45e759
Dan Fan <fandan.nyc@gmail.com>
2017-06-08 00:10:57 -0400

    27 easy

--- java/src/main/removeElement/RemoveElement.java
+++ java/src/main/removeElement/RemoveElement.java
@@ -1,13 +1,12 @@
 public class Solution {
     public int removeElement(int[] nums, int val) {
-        int x = -1;
-        for(int i= 0; i < nums.length; i++)
-        {
-            if(nums[i] != val)
-            {
-                nums[++x] = nums[i];
+        int next = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != val){
+                nums[next] = nums[i];
+                next++;
             }
         }
-        return x+1 ;
+        return next;
     }
 }
