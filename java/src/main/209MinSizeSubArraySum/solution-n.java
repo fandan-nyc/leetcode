9e8e5c424770f26305d335e410ca67f6ee5b4212
Dan Fan <fandan.nyc@gmail.com>
2017-06-21 21:19:48 -0400

    Update solution-n.java

--- java/src/main/209MinSizeSubArraySum/solution-n.java
+++ java/src/main/209MinSizeSubArraySum/solution-n.java
@@ -1,20 +1,17 @@
-class Solution {
+public class Solution {
     public int minSubArrayLen(int s, int[] nums) {
-        int sum = 0; 
-        int min = Integer.MAX_VALUE;
-        int start = 0 ;
-        int end  = 0 ;
-        for(int i = 0; i < nums.length;i++){
-            sum += nums[i];
-            end++;
-            if(sum >= s){
-                while(sum >= s){
-                    sum -= nums[start];
-                    start++;
-                }
-                min = (min < end-start+1) ? min: (end-start+1);
+        int min = nums.length+1;
+        int sum = 0;
+        int start = 0;
+        int end = 0;
+        while(end < nums.length){
+            sum += nums[end];
+            while(sum >= s){
+                min = Math.min(min, end-start +1);
+                sum -= nums[start++];
             }
+            end++;
         }
-        return min == Integer.MAX_VALUE ? 0 : min ;
+        return min == nums.length+1 ? 0: min;
     }
 }
