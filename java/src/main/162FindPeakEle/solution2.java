bdc0381f289f7037a80b9bde8dc8009608fee52f
Dan Fan <fandan.nyc@gmail.com>
2017-07-21 15:57:08 -0400

    Update solution2.java

--- java/src/main/162FindPeakEle/solution2.java
+++ java/src/main/162FindPeakEle/solution2.java
@@ -18,3 +18,20 @@ public class Solution {
         return nums[0] > nums[1] ? 0: nums.length-1;
     }
 }
+
+/// 
+public class Solution {
+    public int findPeakElement(int[] nums) {
+        int left  = 0; 
+        int right = nums.length -1; 
+        while(left < right){
+            int middle = left + (right - left) /2; 
+            if(nums[middle] < nums[middle+1]){
+                left = middle+1;
+            }else{
+                right = middle;
+            }
+        }
+        return left;
+    }
+}
