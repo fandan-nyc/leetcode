b0a402456cd121cf3ffc20e17cf004657636bf8e
Dan Fan <fandan.nyc@gmail.com>
2017-07-29 04:38:31 -0400

    Update solution3.java

--- java/src/main/longestPalindromeSubstring/solution3.java
+++ java/src/main/longestPalindromeSubstring/solution3.java
@@ -1,3 +1,32 @@
+public class Solution {
+    public String longestPalindrome(String s) {
+        if(s == null || s.length() <= 1){
+            return s;
+        }
+        int max  = 1 ;
+        String res = s.substring(0,1);
+        for(int i = 0; i < s.length(); i++){
+            int oddLen  = palinHelper(s, i-1, i+1, 1);
+            int evenLen = palinHelper(s, i, i+1, 0);
+            int currMax = Math.max(oddLen, evenLen);
+            if(currMax > max){
+                max = currMax;
+                res = s.substring(i-currMax/2 + (currMax%2 == 0 ? 1:0), i+currMax/2+1);
+            }
+        }
+        return res;
+    }
+    
+    private int palinHelper(String s, int start, int end, int currLen){
+        while(start >= 0 && end <= s.length()-1 && s.charAt(start) == s.charAt(end)){
+            currLen += 2;   
+            start--;
+            end++;
+        }
+        return currLen;
+    }
+}
+
 // time: O(n^2) space O(1)
 public class Solution {
     public String longestPalindrome(String s) {
