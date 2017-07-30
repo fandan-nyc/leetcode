2295ef6b14f08e5bd0c6aa40f0cc300752d95864
Dan Fan <fandan.nyc@gmail.com>
2017-07-30 19:36:54 -0400

    Update UniquePathII.java

--- java/src/main/uniquePathII/UniquePathII.java
+++ java/src/main/uniquePathII/UniquePathII.java
@@ -1,23 +1,17 @@
 public class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        final int m =  obstacleGrid.length;
-        final int n = obstacleGrid[0].length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
-        for(int i = 0; i < obstacleGrid[0].length; i++){
-            if(obstacleGrid[0][i] == 1 || (i > 0 && dp[i-1]== 0)){
-                dp[i]= 0;
-            }
-        }
-        for(int i = 1; i < m; i ++){
-            for(int j = 0; j < n; j++){
-                if(obstacleGrid[i][j] == 1){
-                    dp[j] = 0;
+        int col = obstacleGrid[0].length-1;
+        int[] total = new int[col+1];
+        total[0] = 1;
+        for(int[] row :obstacleGrid ){
+            for(int j = 0; j <= col; j++){
+                if(row[j] ==  1){
+                    total[j] = 0;
                 }else if(j > 0){
-                    dp[j] = dp[j-1] + dp[j];
+                    total[j] += total[j-1];
                 }
             }
         }
-        return dp[n-1];
+        return total[col];
     }
 }
