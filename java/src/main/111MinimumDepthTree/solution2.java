700ab80cb1c68fe1260394176fa5adc46a8752a0
Dan Fan <dfan@appnexus.com>
2017-08-05 16:10:13 -0400

    111 easy

---
+++ java/src/main/111MinimumDepthTree/solution2.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        if(root.left == null || root.right ==null){
+            return 1+Math.max(minDepth(root.left), minDepth(root.right));
+        }
+        return 1+Math.min(minDepth(root.left), minDepth(root.right));
+    }
+}
