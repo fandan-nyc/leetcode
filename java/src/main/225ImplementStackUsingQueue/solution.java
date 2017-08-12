8905ecd840c17ddd6a7db0db37be9eb21493db19
Dan Fan <fandan.nyc@gmail.com>
2017-08-12 16:28:20 -0400

    Update solution.java

--- java/src/main/225ImplementStackUsingQueue/solution.java
+++ java/src/main/225ImplementStackUsingQueue/solution.java
@@ -1,41 +1,46 @@
 public class MyStack {
 
     /** Initialize your data structure here. */
-    Queue<Integer> first ;
-    Queue<Integer> second ;
+    Queue<Integer> first; 
+    Queue<Integer> second;
+    Integer top;
     public MyStack() {
         first = new LinkedList<>();
         second = new LinkedList<>();
+        top = null;
     }
     
     /** Push element x onto stack. */
     public void push(int x) {
-        if(!first.isEmpty()){
-            second.add(first.remove());
-        }
+        // push is O(1)
         first.add(x);
+        top = x;
     }
     
     /** Removes the element on top of the stack and returns that element. */
     public int pop() {
-        int result = first.remove();
-        while(second.size() > 1){
-            first.add(second.remove());
+        // pop is O(n)
+        // iterate all the way to the tail to pop
+        while(first.size() > 1){
+            top = first.remove();
+            second.add(top);
         }
+        int res = first.remove();
         Queue<Integer> tmp = first;
-        first = second; 
+        first= second; 
         second = tmp;
-        return result;
+        return res;
+        
     }
     
     /** Get the top element. */
     public int top() {
-        return first.peek();
+        return top;
     }
     
     /** Returns whether the stack is empty. */
     public boolean empty() {
-        return first.isEmpty() && second.isEmpty() ; 
+        return first.isEmpty() && second.isEmpty();
     }
 }
 
