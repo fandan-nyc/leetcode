273223dec524f3dcf160344931995c99d5ffa893
Dan Fan <dfan@appnexus.com>
2017-08-12 16:09:24 -0400

    225

---
+++ java/src/main/225ImplementStackUsingQueue/solution3.java
@@ -0,0 +1,43 @@
+public class MyStack {
+
+    /** Initialize your data structure here. */
+    Queue<Integer> queue;
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+    
+    /** Push element x onto stack. */
+    public void push(int x) {
+        queue.add(x);
+        int size = queue.size();
+        while(size > 1){
+            queue.add(queue.remove());
+            size--;
+        }
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return queue.remove();
+    }
+    
+    /** Get the top element. */
+    public int top() {
+        return queue.peek();
+    }
+    
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return queue.isEmpty();
+        
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
