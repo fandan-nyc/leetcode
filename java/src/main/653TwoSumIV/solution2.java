f1be5c5fe7e0b0f54ebb6e974498605c4f721f57
Dan Fan <dfan@appnexus.com>
2017-08-17 22:29:15 -0400

    653

---
+++ java/src/main/653TwoSumIV/solution2.java
@@ -0,0 +1,24 @@
+ublic class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        List < Integer > list = new ArrayList();
+        inorder(root, list);
+        int l = 0, r = list.size() - 1;
+        while (l < r) {
+            int sum = list.get(l) + list.get(r);
+            if (sum == k)
+                return true;
+            if (sum < k)
+                l++;
+            else
+                r--;
+        }
+        return false;
+    }
+    public void inorder(TreeNode root, List < Integer > list) {
+        if (root == null)
+            return;
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+    }
+}
