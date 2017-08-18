c8fdc3031bcaf37301f28c1589d1cced0b49a33d
Dan Fan <fandan.nyc@gmail.com>
2017-08-17 20:22:58 -0400

    Update recursive.java

--- java/src/main/236LowestAncestorOfABinaryTree/recursive.java
+++ java/src/main/236LowestAncestorOfABinaryTree/recursive.java
@@ -7,23 +7,17 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-public class Solution {
+class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null){
+        // bottom up, O(n)
+        if(root ==null || root == p || root ==  q){
             return root;
-        }
-        if(root == p){
-            return p;
-        }
-        if(root == q){
-            return q;
         }
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
         if(left != null && right != null){
             return root;
-        }else{
-            return left == null? right: left;
         }
+        return left != null ? left: right;
     }
 }
