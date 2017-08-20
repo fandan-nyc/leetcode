872b21b9192882421d886cf8bc58e67bbd3899ff
Dan Fan <fandan.nyc@gmail.com>
2017-08-20 11:09:57 -0400

    494 hard DP -- use sum <= 1000

--- java/src/main/494targetSum/solution3.java
+++ java/src/main/494targetSum/solution3.java
@@ -1,23 +1,19 @@
-public class Solution {
-    private int[][] store ;
+class Solution {
     public int findTargetSumWays(int[] nums, int S) {
-       // DP
-       LinkedList<Integer> queue = new LinkedList<>();
-       int[][] dp = new int[nums.length][2001];
-       dp[0][nums[0]+1000] +=1;
-       dp[0][-nums[0]+1000] +=1;
-       for(int i = 1; i < nums.length; i++){
-           for(int j =0; j < 2001; j++){
-               if(dp[i-1][j] != 0){
-                    int prevCount = dp[i-1][j];
-                    dp[i][j + nums[i]] += prevCount;
-                    dp[i][j - nums[i]] += prevCount;
+        if(S > 1000 || S < -1000){
+            return 0;
+        }
+        int[][] dp = new int[nums.length][2001];
+        dp[0][nums[0] + 1000] = 1;
+        dp[0][-nums[0] + 1000] += 1;
+        for(int i = 1; i < nums.length; i++){
+            for(int sum = -1000; sum <= 1000; sum++){
+                if(dp[i-1][sum+1000] > 0){
+                    dp[i][sum + nums[i] + 1000] += dp[i-1][sum + 1000];
+                    dp[i][sum - nums[i] + 1000] += dp[i-1][sum + 1000];
                 }
-           }
-       }
-       if( S > 1000 || S < -1000){
-           return 0;
-       }
-       return dp[nums.length-1][S+1000];
+            }
+        }
+        return dp[nums.length-1][S+1000];
     }
 }
