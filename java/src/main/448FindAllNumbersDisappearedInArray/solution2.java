268f6ce471a9f54d47b10f6ca2673ed8bedf8868
Dan Fan <fandan.nyc@gmail.com>
2017-08-21 21:18:16 -0400

    Update solution2.java

--- java/src/main/448FindAllNumbersDisappearedInArray/solution2.java
+++ java/src/main/448FindAllNumbersDisappearedInArray/solution2.java
@@ -1,26 +1,15 @@
-public class Solution {
+class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        // with space
         for(int i = 0; i < nums.length; i++){
-            int curr = nums[i];
-            if(curr != -1 && nums[curr-1] != -1){
-                swap(nums, i, curr-1);
-                nums[curr-1] = -1;
-                i--;
-            }
+            int tmp = Math.abs(nums[i])-1;
+            nums[tmp] = -1 * Math.abs(nums[tmp]);
         }
         List<Integer> res = new ArrayList<>();
-        for(int i = 0; i < nums.length;i++){
-            if(nums[i] != -1){
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > 0){
                 res.add(i+1);
             }
         }
         return res;
-    }
-    
-    private void swap(int[] nums, int a, int b){
-        int tmp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = tmp;
     }
 }
