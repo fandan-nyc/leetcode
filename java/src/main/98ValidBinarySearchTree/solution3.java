03ff4df0c79e3484c827ce0ac312d2718720dba4
Dan Fan <fandan.nyc@gmail.com>
2017-08-23 22:10:33 -0400

    Create solution3.java

---
+++ java/src/main/98ValidBinarySearchTree/solution3.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    private boolean helper(TreeNode root, long min, long max){
+        if(root == null){
+            return true;
+        }
+        if(root.val <= min || root.val >= max ){
+            return false;
+        }
+        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+    }
+}
