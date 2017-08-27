ede2653e67cf489fde55994347096fb1cd2462ee
Dan Fan <dfan@appnexus.com>
2017-08-27 15:20:46 -0400

    253 medium

---
+++ java/src/main/253MeetingRoomsII/solution1.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+class Solution {
+    public int minMeetingRooms(Interval[] intervals) {
+        
+        int[] start = new int[intervals.length];
+        int[] end = new int[intervals.length];
+     
+        for(int i  = 0; i < intervals.length; i++){
+            start[i] = intervals[i].start;
+            end[i]   = intervals[i].end;
+        }
+        Arrays.sort(start);
+        Arrays.sort(end);
+        int startMeeting = 0;
+        int endMeeting = 0;
+        int room = 0;
+        for(int i = 0; i < intervals.length; i++){
+            if(start[i] < end[endMeeting]){
+                room++;
+            }else{
+                endMeeting++;
+            }
+        }
+        return room;
+    }
+}
