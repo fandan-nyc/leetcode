46f677cfdf98d5c515d0a343756cdd3aa27216f7
Dan Fan <fandan.nyc@gmail.com>
2017-08-29 09:02:59 -0400

    Update 38CountAndSay.java

--- java/src/main/38CountAndSay/38CountAndSay.java
+++ java/src/main/38CountAndSay/38CountAndSay.java
@@ -1,30 +1,26 @@
-public class Solution {
+class Solution {
     public String countAndSay(int n) {
-        String result = "1";
-        while(n > 1)
-        {
-            result = next(result);
-            n --;
+        if(n <= 0){
+            return null;
         }
-        return result;
+        StringBuilder sb = new StringBuilder();
+        sb.append("1");
+        for(int j = 1; j < n; j++){
+            sb = helper(sb);
+        }
+        return sb.toString();
     }
     
-    private String next(String i)
-    {
-        char tmp;
-        int count = 0;
-        String result = "";
-        for(int k = 0;k < i.length();)
-        {
-            tmp = i.charAt(k);
-            while(k < i.length() &&  i.charAt(k) == tmp)
-            {
-                count +=1;
-                k++;
+    private StringBuilder helper(StringBuilder input){
+        StringBuilder res = new StringBuilder();
+        for(int i = 0; i < input.length() ; i++){
+            int count = 1;
+            while(i < input.length()-1 && input.charAt(i) == input.charAt(i+1)){
+                i++;
+                count++;
             }
-            result = result + (Integer.toString(count) + tmp)  ;
-            count = 0;
+            res.append(count + "" + input.charAt(i));
         }
-        return result ; 
+        return res;
     }
 }
