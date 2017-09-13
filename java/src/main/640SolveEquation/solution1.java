b270740162760697cc833b5938212bbfeaa0d3c0
Dan Fan <fandan.nyc@gmail.com>
2017-09-13 00:04:20 -0400

    Update solution1.java

--- java/src/main/640SolveEquation/solution1.java
+++ java/src/main/640SolveEquation/solution1.java
@@ -1,38 +1,42 @@
-public class Solution {
+class Solution {
     public String solveEquation(String equation) {
-        // validate input 
-        if(equation == null | equation.length() < 3){
-            throw new IllegalArgumentException("illegal argument exception");
-        }
-        int equal = equation.lastIndexOf('=');
-        int[] left = simplify(equation.substring(0, equal));
-        int[] right = simplify(equation.substring(equal+1, equation.length()));
-        int slop = left[0] - right[0];
-        int intersect = right[1] - left[1];
-        if(slop == 0 && intersect == 0){
-            return "Infinite solutions";
-        }else if(slop == 0 && intersect != 0){
+        String[] equs = equation.split("=");
+        String leftStr  = equs[0];
+        String rightStr = equs[1];
+        int[] left  = calculate(leftStr);
+        int[] right = calculate(rightStr);
+        int coef = left[0] - right[0];
+        int intercept = right[1] - left[1];
+        if(coef == 0 && intercept != 0){
             return "No solution";
+        }else if(coef  == 0 && intercept == 0){
+            return "Infinite solutions";
         }else{
-            return "x=" + intersect/slop;
+            System.out.println(intercept);
+            System.out.println(coef);
+
+            return "x=" + (intercept/coef);
         }
     }
     
-    private int[] simplify(String exp){
-        String[] each = exp.split("(?=[+-])");
-        int slop = 0;
-        int intersect = 0 ;
-        for(String token: each){
-            if(token.equals("x") || token.equals("+x")){
-                slop += 1;
-            }else if (token.equals("-x")){
-                slop -= 1;
-            }else if(token.contains("x")){
-                slop += Integer.parseInt(token.substring(0,token.length()-1));
+    private String coeff(String x){
+        if(x.length() > 1 && x.charAt(x.length()-2) >= '0' && x.charAt(x.length()-2) <= '9'){
+            return x.replace("x","");
+        }
+        return x.replace("x","1");
+    }
+    
+    private int[] calculate(String str){
+        int coef = 0; 
+        int intercept = 0;
+        String[] strSplit = str.split("(?=[+-])");
+        for(String i: strSplit){
+            if(i.contains("x")){
+                coef += Integer.valueOf(coeff(i));
             }else{
-                intersect += Integer.parseInt(token.substring(0,token.length()));
+                intercept += Integer.valueOf(i);
             }
         }
-        return new int[]{slop, intersect};
+        return new int[]{coef, intercept};
     }
 }
