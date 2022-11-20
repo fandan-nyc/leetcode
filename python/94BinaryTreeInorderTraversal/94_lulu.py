794d35479a6eb1d3fc3ab8878b93111e95dbfd03
Lulu Sun <sunrey32@uw.edu>
2022-11-19 22:01:49 -0800

    94, python, recur

---
+++ python/94BinaryTreeInorderTraversal/94_lulu.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        result = []
+        self.helper(root, result)
+        return result
+        
+    def helper(self, root, result):
+        if root:
+            self.helper(root.left, result)
+            result.append(root.val)
+            self.helper(root.right, result)
