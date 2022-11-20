a6a83990dc8c1808e6658951e0590d526fdc4e0c
SitongChe <chesitong@126.com>
2022-11-19 22:25:47 -0800

    python

---
+++ python/102BinaryTreeLevelOrderTraversal/queue_python.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        result = []
+        if not root:
+            return result
+
+        queue = [root]
+        while(len(queue)): 
+            size = len(queue)
+            level = []
+            for i in range(0,size):
+                cur = queue.pop(0)
+                level.append(cur.val)
+                if cur.left:
+                    queue.append(cur.left)
+                if cur.right:
+                    queue.append(cur.right)
+            result.append(level)
+
+        return result
+
+
